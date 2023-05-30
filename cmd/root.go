eb4c99baea0259a7412d87c017f6d5c66ca6ca3a
Andrii Klymiuk <gradli.inc@gmail.com>
2023-05-30 10:30:45 +0300

    Fix warning

--- cmd/root.go
+++ cmd/root.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var APP_VERSION = "1.1.61"
+var APP_VERSION = "1.1.62"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
