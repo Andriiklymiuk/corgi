3dc563d4e7ca6ad5eed7ced91eb2812c1284da95
Andrii Klymiuk <gradli.inc@gmail.com>
2023-06-09 22:11:43 +0300

    Added new lines

--- cmd/root.go
+++ cmd/root.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var APP_VERSION = "1.1.65"
+var APP_VERSION = "1.1.66"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
