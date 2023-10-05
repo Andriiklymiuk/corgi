f6ab221f0aae7be10fedfa28e503c967bcb26334
Andrii Klymiuk <gradli.inc@gmail.com>
2023-10-05 15:51:42 +0300

    Bump version

--- cmd/root.go
+++ cmd/root.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var APP_VERSION = "1.1.90"
+var APP_VERSION = "1.1.91"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
