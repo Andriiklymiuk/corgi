7544e2f12ac307f0b819cd296384c026c6eac8d1
Andrii Klymiuk <gradli.inc@gmail.com>
2023-10-10 22:23:26 +0300

    Added missing keydb

--- cmd/root.go
+++ cmd/root.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var APP_VERSION = "1.1.98"
+var APP_VERSION = "1.1.99"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
