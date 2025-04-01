f34d7d62e86b9a96c199adcdae28a7632f1483b8
Andrii Klymiuk <ext-andrii.klymiuk@skeepers.io>
2025-04-01 21:09:53 +0300

    Update corgi version

--- cmd/root.go
+++ cmd/root.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var APP_VERSION = "1.7.7"
+var APP_VERSION = "1.8.0"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
