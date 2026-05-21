078ff50fa21a45f1364d248767e82c9919a83424
Andrii Klymiuk <ext-andrii.klymiuk@skeepers.io>
2026-05-21 23:53:36 +0300

    chore: stop signal notify in status watch tty

--- cmd/status.go
+++ cmd/status.go
@@ -257,6 +257,7 @@ func runStatusWatchTTY(rows []statusRow, interval time.Duration) {
 
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(sigCh)
 	go func() {
 		<-sigCh
 		restore()
