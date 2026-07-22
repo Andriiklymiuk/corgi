c4c530e1dc293aea58e92e27b4283f0b70398836
Andrii Klymiuk <ext-andrii.klymiuk@skeepers.io>
2026-07-22 13:51:32 +0300

    +

--- cmd/root.go
+++ cmd/root.go
@@ -8,7 +8,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var APP_VERSION = "1.20.17"
+var APP_VERSION = "1.20.18"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
