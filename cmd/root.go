a9ae914e964c80059322e8862dbf8465e1762293
Andrii Klymiuk <gradli.inc@gmail.com>
2026-08-26 08:46:30 +0300

    +

--- cmd/root.go
+++ cmd/root.go
@@ -8,7 +8,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var APP_VERSION = "1.20.37"
+var APP_VERSION = "1.20.38"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
