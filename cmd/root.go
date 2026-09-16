80c96d854e206f3aa975e3ec8adf750aca88634b
Andrii Klymiuk <gradli.inc@gmail.com>
2026-09-16 15:33:30 +0300

    release: 2.28.11 — the Proactive bot

--- cmd/root.go
+++ cmd/root.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var APP_VERSION = "2.28.10"
+var APP_VERSION = "2.28.11"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
