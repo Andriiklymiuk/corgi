40a30275d73286da71ba51d0652227d4a278c3a7
Andrii Klymiuk <gradli.inc@gmail.com>
2026-09-17 09:09:48 +0300

    release: 2.28.14 — unattended on a server

--- cmd/root.go
+++ cmd/root.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var APP_VERSION = "2.28.13"
+var APP_VERSION = "2.28.14"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
